d60606591491f31a7dec3c3d3e41467ae61609c5
Yumihoshi <xiaoyuesun915@gmail.com>
2025-02-17 14:08:19 +0800

    [change] 框架插件版本更新
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

---
+++ Plugins/LumiverseFramework-v1.0.3/LumiVerseFramework/Base/FSM/FsmNode.cs
@@ -0,0 +1,29 @@
+﻿// *****************************************************************************
+// @author: 绘星tsuki
+// @email: xiaoyuesun915@gmail.com
+// @creationDate: 2025/02/16 15:02
+// @version: 1.0
+// @description: 状态机节点基类
+// *****************************************************************************
+
+using System;
+using Godot;
+
+namespace LumiVerseFramework.Base.FSM;
+
+public abstract partial class FsmNode<TStateType> : Node where TStateType : Enum
+{
+    public Fsm<TStateType> Fsm { get; } = new();
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        Fsm.curState.OnProcess();
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        Fsm.curState.OnPhysicsProcess();
+    }
+}
