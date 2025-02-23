71aa53d9fedcc9ae52562e533e3c4a98926317cf
Yumihoshi <xiaoyuesun915@gmail.com>
2025-02-23 16:56:10 +0800

    [add] 玩家血量
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

--- Scripts/MVC/Models/Player/PlayerModel.cs
+++ Scripts/MVC/Models/Player/PlayerModel.cs
@@ -6,6 +6,7 @@
 // @description:
 // *****************************************************************************
 
+using System.ComponentModel;
 using Godot;
 
 namespace PentiumCup2025.Scripts.MVC.Models.Player;
@@ -15,21 +16,53 @@ public partial class PlayerModel : Resource
     /// <summary>
     /// 移动速度
     /// </summary>
-    [ExportGroup("玩家属性")]
+    [ExportGroup("玩家速度")]
     [Export]
-    public float Speed { get; set; }
+    public float Speed { get; set; } = 3000;
 
     /// <summary>
     /// 转向速度
     /// </summary>
     [Export]
-    public float RotateSpeed { get; set; }
+    public float RotateSpeed { get; set; } = 1;
 
     /// <summary>
     /// 加速叠加的速度
     /// </summary>
     [Export]
-    public float SpeedUpAddValue { get; set; }
+    public float SpeedUpAddValue { get; set; } = 1500;
+
+    [ExportGroup("玩家血量")]
+    [Export]
+    public int MaxHealth { get; private set; } = 100;
+
+    public int CurrentHealth { get; set; }
 
     [ExportGroup("子弹")] [Export] public PackedScene Firework { get; set; }
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Init()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    /// <summary>
+    /// 受伤
+    /// </summary>
+    /// <param name="damage"></param>
+    public void Damage(int damage)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth - damage, 0, MaxHealth);
+    }
+
+    /// <summary>
+    /// 治疗
+    /// </summary>
+    /// <param name="heal"></param>
+    public void Heal(int heal)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth + heal, 0, MaxHealth);
+    }
 }
